cad816eb90a0922a3b94987dca20d3693b6acd35
mbrookhart <mbrookhart@octoml.ai>
2020-12-14 22:29:36 -0700

    convert loop indices to int64 to prevent overflow in start calculation

--- tests/python/relay/test_op_level6.py
+++ tests/python/relay/test_op_level6.py
@@ -36,15 +36,15 @@ def test_argsort():
             ref_res = np.argsort(-x_data, axis=axis, kind="stable")
 
         for target, ctx in tvm.testing.enabled_targets():
-            if target == "nvptx":
-                continue
             for kind in ["graph", "debug"]:
-                intrp = relay.create_executor(kind, ctx=ctx, target=target)
-                op_res = intrp.evaluate(func)(x_data)
+                mod = tvm.IRModule.from_expr(func)
+                intrp = relay.create_executor(kind, mod=mod, ctx=ctx, target=target)
+                op_res = intrp.evaluate()(x_data)
                 tvm.testing.assert_allclose(op_res.asnumpy(), ref_res.astype(dtype), rtol=1e-5)
 
     for dtype in ["int32", "int64", "float32", "float64"]:
         verify_argsort((2, 3, 4), axis=0, is_ascend=False, dtype=dtype)
+        verify_argsort((1, 122640), axis=1, is_ascend=False, dtype=dtype)
         verify_argsort((1, 4, 6), axis=1, is_ascend=True, dtype=dtype)
         verify_argsort((3, 5, 6), axis=-1, is_ascend=False, dtype=dtype)
         verify_argsort((3, 2000, 6), axis=1, is_ascend=False, dtype=dtype)
