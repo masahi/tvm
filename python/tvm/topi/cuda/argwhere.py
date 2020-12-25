0492fb10edf7bd6e3ab497a3414f3e223c48bfd1
Masahiro Masuda <masahi129@gmail.com>
2020-12-25 11:05:35 +0900

    refactoring

--- python/tvm/topi/cuda/argwhere.py
+++ python/tvm/topi/cuda/argwhere.py
@@ -21,49 +21,34 @@ import logging
 
 import tvm
 from tvm import te
-from tvm._ffi import get_global_func
 from .injective import schedule_injective_from_existing
-from .nms import atomic_add
-from .sort import topk, topk_thrust, argsort, argsort_thrust
 from .scan import exclusive_scan
 from .. import tag
 from ..utils import ceil_div, prod
-from ..transform import strided_slice, adv_index, squeeze, reshape
+from ..transform import reshape
 from ..broadcast import not_equal
 from ..math import cast
 
 
 logger = logging.getLogger("topi")
 
-
-def _get_sort_func(mode=0):
-    """Get sort function for argwhere. mode 0 for topk and others for argsort."""
-    if get_global_func("tvm.contrib.thrust.sort", allow_missing=True):
-        ret = topk_thrust if mode == 0 else argsort_thrust
-    else:
-        logger.warning(
-            "It's highly recommended to enable thrust library with set(USE_THRUST ON)"
-            " when compiling argwhere for cuda target. Otherwise, it can result in"
-            " significant performance degradation or incorrect result"
-        )
-        ret = topk if mode == 0 else argsort
-
-    return ret
+fdiv = tvm.tir.floordiv
+fmod = tvm.tir.floormod
 
 
-def compact_nonzero_indices_1d_ir(condition, write_indices, out):
+def compact_nonzero_indices_ir(condition, write_indices, out, do_write_func):
     """
     TODO
     """
     ib = tvm.tir.ir_builder.create()
-    a0 = prod(condition.shape)
+    size_1d = prod(condition.shape)
 
     condition = ib.buffer_ptr(condition)
     write_indices = ib.buffer_ptr(write_indices)
     out = ib.buffer_ptr(out)
 
     nthread_tx = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_bx = ceil_div(a0, nthread_tx)
+    nthread_bx = ceil_div(size_1d, nthread_tx)
     tx = te.thread_axis("threadIdx.x")
     bx = te.thread_axis("blockIdx.x")
     ib.scope_attr(tx, "thread_extent", nthread_tx)
@@ -71,11 +56,38 @@ def compact_nonzero_indices_1d_ir(condition, write_indices, out):
 
     with ib.new_scope():
         idx = bx * nthread_tx + tx
-        with ib.if_scope(idx < a0):
+        with ib.if_scope(idx < size_1d):
             with ib.if_scope(condition[idx] != 0):
-                out[write_indices[idx]] = idx
+                do_write_func(out, write_indices[idx], idx)
 
     return ib.get()
+
+
+def argwhere_common(output_shape, condition, do_write_func):
+    flags = not_equal(condition, tvm.tir.const(0))
+    flags_1d = reshape(flags, (prod(flags.shape),))
+    write_indices = exclusive_scan(cast(flags_1d, dtype="int32"))
+
+    condition_buf = tvm.tir.decl_buffer(
+        condition.shape, condition.dtype, "data_buf", data_alignment=8
+    )
+    write_indices_buf = tvm.tir.decl_buffer(
+        write_indices.shape, write_indices.dtype, "write_indices_buf", data_alignment=8
+    )
+    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
+
+    out = te.extern(
+        [output_shape],
+        [condition, write_indices],
+        lambda ins, outs: compact_nonzero_indices_ir(ins[0], ins[1], outs[0], do_write_func),
+        dtype=["int32"],
+        in_buffers=[condition_buf, write_indices_buf],
+        out_buffers=[out_buf],
+        name="argwhere",
+        tag="argwhere_gpu",
+    )
+
+    return out
 
 
 def argwhere_1d(output_shape, condition):
@@ -94,110 +106,10 @@ def argwhere_1d(output_shape, condition):
     stmt : Stmt
         The result IR statement.
     """
-    flags = not_equal(condition, tvm.tir.const(0))
-    write_indices = exclusive_scan(cast(flags, dtype="int32"))
+    def do_write(out, write_index, idx):
+        out[write_index] = idx
 
-    condition_buf = tvm.tir.decl_buffer(
-        condition.shape, condition.dtype, "data_buf", data_alignment=8
-    )
-
-    write_indices_buf = tvm.tir.decl_buffer(
-        write_indices.shape, write_indices.dtype, "write_indices_buf", data_alignment=8
-    )
-    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
-
-    out = te.extern(
-        [output_shape],
-        [condition, write_indices],
-        lambda ins, outs: compact_nonzero_indices_1d_ir(ins[0], ins[1], outs[0]),
-        dtype=["int32"],
-        in_buffers=[write_indices_buf, condition_buf],
-        out_buffers=[out_buf],
-        name="argwhere_1d",
-        tag="argwhere1d_gpu",
-    )
-
-    return out
-
-
-def compact_nonzero_indices_2d_ir(condition, write_indices, out):
-    """
-    TODO
-    """
-    ib = tvm.tir.ir_builder.create()
-    a1 = condition.shape[1]
-    size_1d = prod(condition.shape)
-
-    condition = ib.buffer_ptr(condition)
-    write_indices = ib.buffer_ptr(write_indices)
-    out = ib.buffer_ptr(out)
-
-    nthread_tx = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_bx = ceil_div(size_1d, nthread_tx)
-    tx = te.thread_axis("threadIdx.x")
-    bx = te.thread_axis("blockIdx.x")
-    ib.scope_attr(tx, "thread_extent", nthread_tx)
-    ib.scope_attr(bx, "thread_extent", nthread_bx)
-
-    with ib.new_scope():
-        idx = bx * nthread_tx + tx
-        with ib.if_scope(idx < size_1d):
-            with ib.if_scope(condition[idx] != 0):
-                out[write_indices[idx] * 2] = tvm.tir.floordiv(idx, a1)
-                out[write_indices[idx] * 2 + 1] = tvm.tir.floormod(idx, a1)
-
-    return ib.get()
-
-
-def argwhere_2d_ir(condition, out):
-    """Low level IR for argwhere 2D
-
-    Parameters
-    ----------
-    condition : Buffer
-        The condition buffer.
-
-    out : Buffer
-        The output buffer.
-
-    Returns
-    -------
-    stmt : Stmt
-        The result IR statement.
-    """
-    ib = tvm.tir.ir_builder.create()
-    a0 = condition.shape[0]
-    a1 = condition.shape[1]
-
-    condition = ib.buffer_ptr(condition)
-    out = ib.buffer_ptr(out)
-
-    valid_index = ib.allocate("int32", (1,), name="valid_index", scope="local")
-    tmp = ib.allocate("int32", (1,), name="tmp", scope="local")
-    one_count = tvm.tir.const(1, dtype="int32")
-
-    max_threads = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_tx = max_threads
-
-    # Limit threads to a single block to make sure atomic_add works normally.
-    tx = te.thread_axis("threadIdx.x")
-    ib.scope_attr(tx, "thread_extent", nthread_tx)
-    len_inner_for = (a0 * a1) // nthread_tx + 1
-
-    valid_index[0] = 0
-
-    with ib.for_range(0, len_inner_for, name="i") as i:
-        idx = tx * len_inner_for + i
-        with ib.if_scope(idx < (a0 * a1)):
-            with ib.if_scope(condition[idx] != 0):
-                tmp[0] = atomic_add(
-                    tvm.tir.call_intrin("handle", "tir.address_of", valid_index[0]),
-                    one_count,
-                )
-                out[tmp[0] * 2] = tvm.tir.floordiv(idx, a1)
-                out[tmp[0] * 2 + 1] = tvm.tir.floormod(idx, a1)
-
-    return ib.get()
+    return argwhere_common(output_shape, condition, do_write)
 
 
 def argwhere_2d(output_shape, condition):
@@ -216,89 +128,12 @@ def argwhere_2d(output_shape, condition):
     stmt : Stmt
         The result IR statement.
     """
-    flags = not_equal(condition, tvm.tir.const(0))
-    flags_1d = reshape(flags, (prod(flags.shape),))
-    write_indices = exclusive_scan(cast(flags_1d, dtype="int32"))
+    def do_write(out, write_index, idx):
+        a1 = condition.shape[1]
+        out[write_index * 2] = tvm.tir.floordiv(idx, a1)
+        out[write_index * 2 + 1] = tvm.tir.floormod(idx, a1)
 
-    condition_buf = tvm.tir.decl_buffer(
-        condition.shape, condition.dtype, "data_buf", data_alignment=8
-    )
-
-    write_indices_buf = tvm.tir.decl_buffer(
-        write_indices.shape, write_indices.dtype, "write_indices_buf", data_alignment=8
-    )
-    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
-
-    out = te.extern(
-        [output_shape],
-        [condition, write_indices],
-        lambda ins, outs: compact_nonzero_indices_2d_ir(ins[0], ins[1], outs[0]),
-        dtype=["int32"],
-        in_buffers=[condition_buf, write_indices_buf],
-        out_buffers=[out_buf],
-        name="argwhere_1d",
-        tag="argwhere1d_gpu",
-    )
-
-    return out
-
-
-def argwhere_3d_ir(condition, out):
-    """Low level IR for argwhere 3D
-
-    Parameters
-    ----------
-    condition : Buffer
-        The condition buffer.
-
-    out : Buffer
-        The output buffer.
-
-    Returns
-    -------
-    stmt : Stmt
-        The result IR statement.
-    """
-    ib = tvm.tir.ir_builder.create()
-    a0 = condition.shape[0]
-    a1 = condition.shape[1]
-    a2 = condition.shape[2]
-    s1 = a1 * a2
-    s0 = a0 * s1
-
-    condition = ib.buffer_ptr(condition)
-    out = ib.buffer_ptr(out)
-
-    valid_index = ib.allocate("int32", (1,), name="valid_index", scope="local")
-    tmp = ib.allocate("int32", (1,), name="tmp", scope="local")
-    one_count = tvm.tir.const(1, dtype="int32")
-
-    max_threads = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_tx = max_threads
-
-    # Limit threads to a single block to make sure atomic_add works normally.
-    tx = te.thread_axis("threadIdx.x")
-    ib.scope_attr(tx, "thread_extent", nthread_tx)
-    len_inner_for = s0 // nthread_tx + 1
-
-    fdiv = tvm.tir.floordiv
-    fmod = tvm.tir.floormod
-
-    valid_index[0] = 0
-
-    with ib.for_range(0, len_inner_for, name="i") as i:
-        idx = tx * len_inner_for + i
-        with ib.if_scope(idx < s0):
-            with ib.if_scope(condition[idx] != 0):
-                tmp[0] = atomic_add(
-                    tvm.tir.call_intrin("handle", "tir.address_of", valid_index[0]),
-                    one_count,
-                )
-                out[tmp[0] * 3] = fdiv(idx, s1)
-                out[tmp[0] * 3 + 1] = fdiv(fmod(idx, s1), a2)
-                out[tmp[0] * 3 + 2] = fmod(idx, a2)
-
-    return ib.get()
+    return argwhere_common(output_shape, condition, do_write)
 
 
 def argwhere_3d(output_shape, condition):
@@ -317,103 +152,14 @@ def argwhere_3d(output_shape, condition):
     stmt : Stmt
         The result IR statement.
     """
-    condition_buf = tvm.tir.decl_buffer(
-        condition.shape, condition.dtype, "data_buf", data_alignment=8
-    )
-    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
+    def do_write(out, write_index, idx):
+        _, a1, a2 = condition.shape
+        s1 = a1 * a2
+        out[write_index * 3] = fdiv(idx, s1)
+        out[write_index * 3 + 1] = fdiv(fmod(idx, s1), a2)
+        out[write_index * 3 + 2] = fmod(idx, a2)
 
-    out = te.extern(
-        [output_shape],
-        [condition],
-        lambda ins, outs: argwhere_3d_ir(ins[0], outs[0]),
-        dtype=["int32"],
-        in_buffers=[condition_buf],
-        out_buffers=[out_buf],
-        name="argwhere_3d",
-        tag="argwhere3d_gpu",
-    )
-
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)) and int(out.shape[0]) <= 1:
-        return out
-
-    # sort the output from the least significant to the most significant
-    # column.
-    sort_func = _get_sort_func(1)
-
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)):
-        for i in reversed(range(3)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-    else:
-        for i in reversed(range(3)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1], [1, 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-    return out
-
-
-def argwhere_4d_ir(condition, out):
-    """Low level IR for argwhere 4D
-
-    Parameters
-    ----------
-    condition : Buffer
-        The condition buffer.
-
-    out : Buffer
-        The output buffer.
-
-    Returns
-    -------
-    stmt : Stmt
-        The result IR statement.
-    """
-    ib = tvm.tir.ir_builder.create()
-    a0 = condition.shape[0]
-    a1 = condition.shape[1]
-    a2 = condition.shape[2]
-    a3 = condition.shape[3]
-    s1 = a2 * a3
-    s2 = a1 * s1
-    s0 = a0 * s2
-
-    condition = ib.buffer_ptr(condition)
-    out = ib.buffer_ptr(out)
-
-    valid_index = ib.allocate("int32", (1,), name="valid_index", scope="local")
-    tmp = ib.allocate("int32", (1,), name="tmp", scope="local")
-    one_count = tvm.tir.const(1, dtype="int32")
-
-    max_threads = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_tx = max_threads
-
-    # Limit threads to a single block to make sure atomic_add works normally.
-    tx = te.thread_axis("threadIdx.x")
-    ib.scope_attr(tx, "thread_extent", nthread_tx)
-    len_inner_for = s0 // nthread_tx + 1
-
-    fdiv = tvm.tir.floordiv
-    fmod = tvm.tir.floormod
-
-    valid_index[0] = 0
-
-    with ib.for_range(0, len_inner_for, name="i") as i:
-        idx = tx * len_inner_for + i
-        with ib.if_scope(idx < s0):
-            with ib.if_scope(condition[idx] != 0):
-                tmp[0] = atomic_add(
-                    tvm.tir.call_intrin("handle", "tir.address_of", valid_index[0]),
-                    one_count,
-                )
-                out[tmp[0] * 4] = fdiv(idx, s2)
-                out[tmp[0] * 4 + 1] = fdiv(fmod(idx, s2), s1)
-                out[tmp[0] * 4 + 2] = fdiv(fmod(idx, s1), a3)
-                out[tmp[0] * 4 + 3] = fmod(idx, a3)
-
-    return ib.get()
+    return argwhere_common(output_shape, condition, do_write)
 
 
 def argwhere_4d(output_shape, condition):
@@ -432,106 +178,16 @@ def argwhere_4d(output_shape, condition):
     stmt : Stmt
         The result IR statement.
     """
-    condition_buf = tvm.tir.decl_buffer(
-        condition.shape, condition.dtype, "data_buf", data_alignment=8
-    )
-    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
+    def do_write(out, write_index, idx):
+        _, a1, a2, a3 = condition.shape
+        s1 = a2 * a3
+        s2 = a1 * s1
+        out[write_index * 4] = fdiv(idx, s2)
+        out[write_index * 4 + 1] = fdiv(fmod(idx, s2), s1)
+        out[write_index * 4 + 2] = fdiv(fmod(idx, s1), a3)
+        out[write_index * 4 + 3] = fmod(idx, a3)
 
-    out = te.extern(
-        [output_shape],
-        [condition],
-        lambda ins, outs: argwhere_4d_ir(ins[0], outs[0]),
-        dtype=["int32"],
-        in_buffers=[condition_buf],
-        out_buffers=[out_buf],
-        name="argwhere_4d",
-        tag="argwhere4d_gpu",
-    )
-
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)) and int(out.shape[0]) <= 1:
-        return out
-
-    # sort the output from the least significant to the most significant
-    # column.
-    sort_func = _get_sort_func(1)
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)):
-        for i in reversed(range(4)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-    else:
-        for i in reversed(range(4)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1], [1, 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-
-    return out
-
-
-def argwhere_5d_ir(condition, out):
-    """Low level IR for argwhere 5D
-
-    Parameters
-    ----------
-    condition : Buffer
-        The condition buffer.
-
-    out : Buffer
-        The output buffer.
-
-    Returns
-    -------
-    stmt : Stmt
-        The result IR statement.
-    """
-    ib = tvm.tir.ir_builder.create()
-    a0 = condition.shape[0]
-    a1 = condition.shape[1]
-    a2 = condition.shape[2]
-    a3 = condition.shape[3]
-    a4 = condition.shape[4]
-    s1 = a3 * a4
-    s2 = a2 * s1
-    s3 = a1 * s2
-    s0 = a0 * s3
-
-    condition = ib.buffer_ptr(condition)
-    out = ib.buffer_ptr(out)
-
-    valid_index = ib.allocate("int32", (1,), name="valid_index", scope="local")
-    tmp = ib.allocate("int32", (1,), name="tmp", scope="local")
-    one_count = tvm.tir.const(1, dtype="int32")
-
-    max_threads = int(tvm.target.Target.current(allow_none=False).max_num_threads)
-    nthread_tx = max_threads
-
-    # Limit threads to a single block to make sure atomic_add works normally.
-    tx = te.thread_axis("threadIdx.x")
-    ib.scope_attr(tx, "thread_extent", nthread_tx)
-    len_inner_for = s0 // nthread_tx + 1
-
-    fdiv = tvm.tir.floordiv
-    fmod = tvm.tir.floormod
-
-    valid_index[0] = 0
-
-    with ib.for_range(0, len_inner_for, name="i") as i:
-        idx = tx * len_inner_for + i
-        with ib.if_scope(idx < s0):
-            with ib.if_scope(condition[idx] != 0):
-                tmp[0] = atomic_add(
-                    tvm.tir.call_intrin("handle", "tir.address_of", valid_index[0]),
-                    one_count,
-                )
-                out[tmp[0] * 5] = fdiv(idx, s3)
-                out[tmp[0] * 5 + 1] = fdiv(fmod(idx, s3), s2)
-                out[tmp[0] * 5 + 2] = fdiv(fmod(idx, s2), s1)
-                out[tmp[0] * 5 + 3] = fdiv(fmod(idx, s1), a4)
-                out[tmp[0] * 5 + 4] = fmod(idx, a4)
-
-    return ib.get()
+    return argwhere_common(output_shape, condition, do_write)
 
 
 def argwhere_5d(output_shape, condition):
@@ -550,42 +206,18 @@ def argwhere_5d(output_shape, condition):
     stmt : Stmt
         The result IR statement.
     """
-    condition_buf = tvm.tir.decl_buffer(
-        condition.shape, condition.dtype, "data_buf", data_alignment=8
-    )
-    out_buf = tvm.tir.decl_buffer(output_shape, "int32", "out_buf", data_alignment=8)
+    def do_write(out, write_index, idx):
+        a0, a1, a2, a3, a4 = condition.shape
+        s1 = a3 * a4
+        s2 = a2 * s1
+        s3 = a1 * s2
+        out[write_index * 5] = fdiv(idx, s3)
+        out[write_index * 5 + 1] = fdiv(fmod(idx, s3), s2)
+        out[write_index * 5 + 2] = fdiv(fmod(idx, s2), s1)
+        out[write_index * 5 + 3] = fdiv(fmod(idx, s1), a4)
+        out[write_index * 5 + 4] = fmod(idx, a4)
 
-    out = te.extern(
-        [output_shape],
-        [condition],
-        lambda ins, outs: argwhere_5d_ir(ins[0], outs[0]),
-        dtype=["int32"],
-        in_buffers=[condition_buf],
-        out_buffers=[out_buf],
-        name="argwhere_5d",
-        tag="argwhere5d_gpu",
-    )
-
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)) and int(out.shape[0]) <= 1:
-        return out
-
-    # sort the output from the least significant to the most significant
-    # column.
-    sort_func = _get_sort_func(1)
-    if isinstance(out.shape[0], (int, tvm.tir.expr.IntImm)):
-        for i in reversed(range(5)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-    else:
-        for i in reversed(range(5)):
-            out1 = strided_slice(out, [0, i], [out.shape[0], i + 1], [1, 1])
-            out2 = sort_func(out1, axis=0, dtype="int32")
-            out3 = squeeze(out2)
-            out = adv_index(out, [out3])
-
-    return out
+    return argwhere_common(output_shape, condition, do_write)
 
 
 def argwhere(output_shape, condition):
